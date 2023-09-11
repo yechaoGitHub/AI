2147a3f12e92243772bc095574b75e60222f8d5a
hxf <hxfengv@163.com>
2023-09-11 22:38:12 +0800

    直接跳转聊天界面

--- widget/chat/WRobotChatMainUI.cpp
+++ widget/chat/WRobotChatMainUI.cpp
@@ -9,7 +9,10 @@ RobotChatMainUI::RobotChatMainUI(QWidget *parent)
     ui.setupUi(this);
     this->setWidgetType(true, DragType::Drag_Null, false);
 
-    ui.stackedWidget->setCurrentWidget(ui.chat_desc_wgt);
+    ui.stackedWidget->setCurrentWidget(ui.chat_widget);
+    ui.lb_title->setText("Chat");
+
+    //ui.stackedWidget->setCurrentWidget(ui.chat_desc_wgt);
     connect(ui.chat_desc_wgt, &WChatDesc::sig_startClick, this, &RobotChatMainUI::StartBtnClicked);
 
     this->setAttribute(Qt::WA_TranslucentBackground, true);
